1df10b33a85a539a68ca75d026ec7b160dd749cd
nusubbotin <nu.subbotin@ya.ru>
2022-10-01 11:31:55 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.time.LocalDate;
+import java.util.EmptyStackException;
 
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     private static void task1() {
@@ -16,5 +18,31 @@ public class Main {
         }else{
             System.out.println(year + " не високосный год");
         }
+    }
+
+    private static void checkProgVersion(int osType, int clientDeviceYear) {
+        String messInfo;
+        if (clientDeviceYear < 2015){
+            messInfo = "Установите облегченную версию приложения для ";
+        }else {
+            messInfo = "Установите стандартную версию приложения для ";
+        }
+        if (osType == 0) {
+            messInfo = messInfo + "iOS";
+        }else if (osType == 1){
+            messInfo = messInfo + "Android";
+        } else {
+            throw new RuntimeException("Не корректные входные данные! Параметр osType может принимать только значения: \n"
+                    + "0 - iOS, 1 - Android");
+        }
+        messInfo = messInfo + " по ссылке";
+        System.out.println(messInfo);
+    }
+
+    private static void task2() {
+        System.out.println("Задание 2:");
+
+        int currentYear = LocalDate.now().getYear();
+        checkProgVersion(0, currentYear);
     }
 }
