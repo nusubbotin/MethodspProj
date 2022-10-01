edb091154f29a0d7cacf52f63104462e9e65709f
nusubbotin <nu.subbotin@ya.ru>
2022-10-01 09:51:22 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    private static void task1() {
+        int currentYear = LocalDate.now().getYear();
+        isLeapYear(currentYear);
+    }
+
+    private static void isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0){
+            System.out.println(year + " — високосный год");
+        }else{
+            System.out.println(year + " не високосный год");
+        }
     }
 }
